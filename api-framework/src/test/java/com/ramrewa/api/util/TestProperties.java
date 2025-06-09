1f9ae49c4fb02137add80bc0926d3fd832ce9612
Abhishek Mishra <abhishek.manipal@gmail.com>
2025-06-09 21:34:35 +1000

    Add the test properties

--- api-framework/src/test/java/com/ramrewa/api/util/TestProperties.java
+++ api-framework/src/test/java/com/ramrewa/api/util/TestProperties.java
@@ -1,4 +1,39 @@
 package com.ramrewa.api.util;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+import java.util.logging.Logger;
 
 public class TestProperties {
+    private static final Properties props;
+    private final static Logger logger = Logger.getLogger("TestProperties.class");
+
+    static {
+        props = new Properties();
+        InputStream input = null;
+        try {
+            String env = System.getProperty("env");
+            if ( env == null) {
+                System.setProperty("env", "staging");
+            } else if ( env.startsWith("@") ) {
+                System.setProperty("env", env.substring(1).toLowerCase());
+            }
+            input = TestProperties.class.getResourceAsStream("/config/" + System.getProperty("env") + ".properties");
+            props.load(input);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } finally {
+            if (input != null) {
+                try {
+                    input.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public static String get(String propertyName) {
+        return props.getProperty(propertyName);
+    }
 }
