8c4191862510bb925e2da17ab32d120e0d45f726
Abhishek Mishra <abhishek.manipal@gmail.com>
2025-06-09 20:49:52 +1000

    Update the restinvocation util

--- api-framework/src/test/java/com/ramrewa/api/util/RestInvocationUtil.java
+++ api-framework/src/test/java/com/ramrewa/api/util/RestInvocationUtil.java
@@ -1,4 +1,60 @@
 package com.ramrewa.api.util;
 
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class RestInvocationUtil {
+    public static String sendGet(String urlStr) throws IOException {
+        URL url = new URL(urlStr);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        conn.setRequestProperty("Accept", "application/json");
+
+        return getResponse(conn);
+    }
+
+    public static String sendPost(String urlStr, String jsonInput) throws IOException {
+        URL url = new URL(urlStr);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("POST");
+        conn.setRequestProperty("Content-Type", "application/json");
+        conn.setDoOutput(true);
+
+        try (OutputStream os = conn.getOutputStream()) {
+            byte[] input = jsonInput.getBytes("utf-8");
+            os.write(input, 0, input.length);
+        }
+
+        return getResponse(conn);
+    }
+    public static String sendPut(String urlStr, String jsonInput) throws IOException {
+        URL url = new URL(urlStr);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("PUT");
+        conn.setRequestProperty("Content-Type", "application/json");
+        conn.setDoOutput(true);
+
+        try (OutputStream os = conn.getOutputStream()) {
+            byte[] input = jsonInput.getBytes("utf-8");
+            os.write(input, 0, input.length);
+        }
+
+        return getResponse(conn);
+    }
+
+    private static String getResponse(HttpURLConnection conn) throws IOException {
+        int status = conn.getResponseCode();
+        InputStream is = (status > 299) ? conn.getErrorStream() : conn.getInputStream();
+        BufferedReader in = new BufferedReader(new InputStreamReader(is));
+        StringBuilder response = new StringBuilder();
+        String line;
+
+        while ((line = in.readLine()) != null) {
+            response.append(line);
+        }
+
+        in.close();
+        return response.toString();
+    }
 }
