38f3845110fca9c50dd9b916e60c9da732b58868
Abhishek Mishra <abhishek.manipal@gmail.com>
2025-06-09 22:28:43 +1000

    Commenting out the BaseHelper unused vars

--- api-framework/src/test/java/com/ramrewa/api/helper/BaseHelper.java
+++ api-framework/src/test/java/com/ramrewa/api/helper/BaseHelper.java
@@ -3,15 +3,15 @@ import java.util.logging.Logger;
 import java.util.LinkedList;
 import java.util.List;
 import com.ramrewa.api.util.TestProperties;
-import io.restassured.http.Header;
+//import io.restassured.http.Header;
 
 public class BaseHelper {
     private final static Logger logger = Logger.getLogger("BaseHelper.class");
-    protected static List<Header> headerListCommon;
+    //protected static List<Header> headerListCommon;
 
     public BaseHelper() {
-        this.headerListCommon = new LinkedList<>();
-        headerListCommon.add(new Header("x-api-key", TestProperties.get("x-api-key")));
-        headerListCommon.add(new Header("Authorization", "Bearer " + sharedData.accessToken));
+        //this.headerListCommon = new LinkedList<>();
+        //headerListCommon.add(new Header("x-api-key", TestProperties.get("x-api-key")));
+        //headerListCommon.add(new Header("Authorization", "Bearer " + sharedData.accessToken));
     }
 }
