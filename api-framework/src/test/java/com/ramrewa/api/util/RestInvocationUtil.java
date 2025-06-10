87cb7d1e5c5db2f71eb607a1dfd4a1d37f6c6f69
Abhishek Mishra <abhishek.manipal@gmail.com>
2025-06-10 13:03:05 +1000

    Make updated to the RestInvocation Util to make it up to mark

--- api-framework/src/test/java/com/ramrewa/api/util/RestInvocationUtil.java
+++ api-framework/src/test/java/com/ramrewa/api/util/RestInvocationUtil.java
@@ -2,9 +2,135 @@ package com.ramrewa.api.util;
 
 import java.io.*;
 import java.net.HttpURLConnection;
+
+import io.restassured.http.Header;
+import io.restassured.http.Headers;
+
 import java.net.URL;
+import java.util.*;
+import java.util.logging.Logger;
+
+import io.restassured.response.Response;
+import junit.framework.Assert;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.baseURI;
 
 public class RestInvocationUtil {
+
+    private final static Logger logger = Logger.getLogger("RestInvocationUtil.class");
+    private final Map<String, String> mapWebServiceResponse = new HashMap<>();
+    public Response response;
+    public ArrayList<String> endPoints = new ArrayList<>();
+    public ArrayList<String> requests = new ArrayList<>();
+    public ArrayList<Response> responses = new ArrayList<>();
+
+    public Map<String, String> invokePostWithHeaders(String endPoint, String requestPayload, List<Header> headerList) {
+        response = postRestWithBodyAndHeaders(endPoint, requestPayload, headerList);
+        String strResponse = response.getBody().asString();
+        mapWebServiceResponse.put("response", strResponse);
+        mapWebServiceResponse.put("statusCode", Integer.toString(response.getStatusCode()));
+        mapWebServiceResponse.put("contentType", (response.contentType()));
+        endPoints.add(endPoint);
+        requests.add(requestPayload);
+        responses.add(response);
+        return mapWebServiceResponse;
+
+    }
+
+    public Map<String, String> invokeGetWithHeaders(String endPoint, Map<String, ?> queryParams, List<Header> headerList) {
+        response = getRestWithDynamicHeaders(endPoint, queryParams, headerList);
+        String strResponse = response.getBody().asString();
+        mapWebServiceResponse.put("response", strResponse);
+        mapWebServiceResponse.put("statusCode", Integer.toString(response.getStatusCode()));
+        mapWebServiceResponse.put("contentType", (response.contentType()));
+        endPoints.add(endPoint);
+        requests.add(queryParams.toString());
+        responses.add(response);
+        return mapWebServiceResponse;
+    }
+
+    public Map<String, String> invokeWithAPIKey(String endPoint, String requestPayload, String apiKey) {
+        response = postRestWithBodyAndAPIkey(endPoint, requestPayload, apiKey);
+        String strResponse = response.getBody().asString();
+        mapWebServiceResponse.put("response", strResponse);
+        mapWebServiceResponse.put("statusCode", Integer.toString(response.getStatusCode()));
+        mapWebServiceResponse.put("contentType", (response.contentType()));
+        endPoints.add(endPoint);
+        requests.add(requestPayload);
+        responses.add(response);
+        return mapWebServiceResponse;
+    }
+
+    private Response postRestWithBodyAndHeaders(String endPoint, String requestPayload, List<Header> dynamicHeaderList) {
+        try {
+            baseURI = getBaseURL(endPoint);
+            Headers headers = new Headers(dynamicHeaderList);
+            response = given()
+                    .header("Content-Type", "application/json")
+                    .header("Accept", "application/json")
+                    .headers(headers)
+                    .body(requestPayload)
+                    .when()
+                    .post(endPoint)
+                    .then()
+                    .extract().response();
+        } catch (Exception e) {
+            e.printStackTrace();
+            Assert.fail("Endpoint::" + endPoint + "Request PayLoad::" + requestPayload + "Error::" + e.getMessage());
+        }
+        return response;
+    }
+
+    private Response getRestWithDynamicHeaders(String endPoint, Map<String, ?> params, List<Header> dynamicHeaderList) {
+        try {
+            baseURI = getBaseURL(endPoint);
+
+            Headers headers = new Headers(dynamicHeaderList);
+            response = given()
+                    .header("Content-Type", "application/json")
+                    .header("Accept", "application/json")
+                    .headers(headers)
+                    .params(params)
+                    .when()
+                    .get(endPoint)
+                    .then()
+                    .extract().response();
+        } catch (Exception e) {
+            e.printStackTrace();
+            StringWriter errors = new StringWriter();
+            e.printStackTrace(new PrintWriter(errors));
+            Assert.fail("Endpoint::" + endPoint + "Error::" + e.getMessage() + "Stack Trace::" + errors.toString());
+        }
+        return response;
+    }
+
+    private Response postRestWithBodyAndAPIkey(String endPoint, String requestPayload, String apiKey) {
+        try {
+            baseURI = getBaseURL(endPoint);
+            List<Header> headerList = new LinkedList<>();
+            headerList.add(new Header("api-key", apiKey));
+            headerList.add(new Header("cache-control", "no-cache"));
+            Headers headers = new Headers(headerList);
+
+            response = given()
+                    .header("Content-Type", "application/json")
+                    .header("Accept", "application/json")
+                    .headers(headers)
+                    .body(requestPayload)
+                    .when()
+                    .post(endPoint).then()
+                    .extract().response();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            StringWriter errors = new StringWriter();
+            e.printStackTrace(new PrintWriter(errors));
+            Assert.fail("Endpoint::" + endPoint + "Request PayLoad::" + requestPayload + "Error::" + e.getMessage() + "Stack Trace::" + errors.toString());
+        }
+        return response;
+    }
+
     public static String sendGet(String urlStr) throws IOException {
         URL url = new URL(urlStr);
         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
@@ -28,6 +154,7 @@ public class RestInvocationUtil {
 
         return getResponse(conn);
     }
+
     public static String sendPut(String urlStr, String jsonInput) throws IOException {
         URL url = new URL(urlStr);
         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
@@ -56,5 +183,10 @@ public class RestInvocationUtil {
 
         in.close();
         return response.toString();
+    }
+
+    private String getBaseURL(String endPoint) {
+        return TestProperties.get("WEATHER_MAP_URL");
+
     }
 }
